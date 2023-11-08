bfde5cab1c5c19f1d14a60f5112fcd5217ab71c5
Ozan Koyuncu <ozankyncu@gmail.com>
2023-11-08 20:14:34 +0300

    State machine actions added

--- src/test/java/com/kyncu/msbeerssm/services/PaymentServiceImplTest.java
+++ src/test/java/com/kyncu/msbeerssm/services/PaymentServiceImplTest.java
@@ -43,7 +43,7 @@ public class PaymentServiceImplTest {
 
         Payment preAuthedPayment = paymentRepository.getOne(savedPayment.getId());
 
-        System.out.println("Should be PRE_AUTH");
+        System.out.println("Should be PRE_AUTH or PRE_AUTH_ERROR");
         System.out.println(sm.getState().getId());
         System.out.println(preAuthedPayment);
     }
